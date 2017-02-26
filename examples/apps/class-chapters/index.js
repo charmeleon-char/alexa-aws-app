999293d20f3896a0235249343df3a8d8c8bc1bae
Oscar Espirilla <oscar.espirilla@laureate.net>
2017-02-26 16:59:56 -0600

    added translation to read-chapter

--- examples/apps/class-chapters/index.js
+++ examples/apps/class-chapters/index.js
@@ -4,13 +4,15 @@ var _ = require('lodash');
 var Alexa = require('alexa-app');
 var app = new Alexa.app('class-chapters');
 var ReadChapter = require('./read-chapter');
+var TranslateHelper = require('./translate-chapter');
+
 
 app.launch(function(req, res) {
   var prompt = 'Please tell me which chapter to read';
   res.say(prompt).reprompt(prompt).shouldEndSession(false);
 });
 
-app.intent('chapter-read', {
+app.intent('chapter_read', {
   'slots': {
     'CHAPTERNUMBER': 'AMAZON.NUMBER'
   },
@@ -36,6 +38,39 @@ app.intent('chapter-read', {
         var prompt = 'I didn\'t have data for chapter ' + chapterNumber;
         res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
       }
+    }
+  }
+);
+
+app.intent('chapter_translate', {
+  'slots': {
+    'CHAPTERNUMBER': 'AMAZON.NUMBER'
+  },
+  'utterances': ['translate {|me} chapter {-|CHAPTERNUMBER}']
+},
+  function(req, res) {
+    //get the slot
+    var chapterNumber = req.slot('CHAPTERNUMBER');
+    var reprompt = 'Tell me the chapter you want me to translate';
+    if (_.isEmpty(chapterNumber)) {
+      var prompt = 'I didn\'t hear a chapter number. Tell me the number';
+      res.say(prompt).reprompt(reprompt).shouldEndSession(false);
+      return true;
+    } else {
+      var readChapter = new ReadChapter();
+      var translateChapter = new TranslateHelper();
+
+      var text = readChapter.getChapterText(chapterNumber);
+
+      translateChapter.translate(text).then(function(response){
+        console.log(response);
+        res.say(response[0]).send();
+      }).catch(function(err) {
+          console.log(err.statusCode);
+          var prompt = 'I didn\'t have data for chapter ' + chapterNumber;
+          res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
+      });
+      return false;
     }
   }
 );
--- examples/apps/class-chapters/translate-chapter.js
+++ examples/apps/class-chapters/translate-chapter.js
@@ -1,3 +1,30 @@
 'use strict';
-var _ = require('lodash');
-var rp = require('request-promise');
+// Imports the Google Cloud client library
+const Translate = require('@google-cloud/translate');
+
+// Your Google Cloud Platform project ID
+const projectId = 'oespirilla';
+
+// Instantiates a client
+const translateClient = Translate({
+  projectId: projectId,
+  keyFilename: './keyfile.json'
+});
+
+function TranslateHelper() {}
+
+TranslateHelper.prototype.translate = function(text) {
+
+    var options = {
+      from: 'en',
+      to: 'es'
+    };
+    return translateClient.translate(text, options).then(
+        function(response) {
+            //console.log(response);
+            return response;
+        }
+    );
+};
+
+module.exports = TranslateHelper;
