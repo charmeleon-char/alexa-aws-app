55fc22760e6abe5ba605fe5b6cce097a334f8af4
Daniel Arita <darita@hnsc.net>
2017-02-26 16:46:56 -0600

    Quiz: Get Questions

--- examples/apps/quiz/index.js
+++ examples/apps/quiz/index.js
@@ -23,6 +23,7 @@ app.intent('quiz', {
         var category = req.slot('CATEGORY');
         if (_.isEmpty(category)) {
             var prompt = 'I didn\'t hear a category name. Tell me the category please Bro';
+            var reprompt = 'Tell me another category please Bro';
             res.say(prompt).reprompt(reprompt).shouldEndSession(false);
             return true;
         } else {
@@ -30,15 +31,18 @@ app.intent('quiz', {
             questions.getByCategory(category).then(function(response) {
                 var quiz = response.body.results;
                 if (_.isEmpty(quiz)) {
-                    var prompt = 'I didn\'t hear a category name. Tell me the category please Bro';
+                    var prompt = 'I didn\'t find any questiosn with that category. Tell me another category please Bro';
+                    var reprompt = 'Tell me another category please Bro';
                     res.say(prompt).reprompt(reprompt).shouldEndSession(false);
                     return true;
-        }
                 } else {
-                    console.log(quiz);
+                    res.session('questions', quiz);
+                    var prompt = 'Say start game to begin.';
                     res.say(quiz[0].type).reprompt(prompt).shouldEndSession(false);
                 }
             });
+            return false;
+        }
     }
 );
 
@@ -47,7 +51,6 @@ app.intent('game', {
         'utterances': ['start game']
     },
     function(req, res) {
-        
     }
 );
 
